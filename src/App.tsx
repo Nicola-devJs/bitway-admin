18812486311eb4c2dceebe078f3fdeef9f6feda5
Nicola-devJs <danil-nikola17@yandex.ru>
2024-06-27 06:27:27 +0300

    change redirect

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,34 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./pages/router";
 import { useLazyGetAuthMeQuery } from "./redux/services/user";
 import { BackdropContext } from "./shared/hoc/BackdropProvider";
-import { getCookie } from "./shared/helpers/cookie";
 
 function App() {
   const [fetcherAuthMe] = useLazyGetAuthMeQuery();
   const { toggleBackdrop } = useContext(BackdropContext);
 
-  useEffect(() => {
-    toggleBackdrop(true);
-    const token = getCookie("token");
+  const fetchAuthMe = async () => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get("token");
 
     if (!token) {
       window.location.replace(import.meta.env.VITE_REDIRECT_HOME);
       return;
     }
 
-    fetcherAuthMe(token)
-      .unwrap()
-      .catch(() => {
-        window.location.replace(import.meta.env.VITE_REDIRECT_HOME);
-      })
-      .finally(() => {
-        toggleBackdrop(false);
-      });
+    try {
+      toggleBackdrop(true);
+
+      await fetcherAuthMe(token).unwrap();
+      params.delete("token");
+    } catch (error) {
+      window.location.replace(import.meta.env.VITE_REDIRECT_HOME);
+    } finally {
+      toggleBackdrop(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAuthMe();
   }, []);
 
   return <RouterProvider router={router} />;
