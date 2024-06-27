3e5f3fca917a38dc4f36042da5e90e082b7a6041
Nicola-devJs <danil-nikola17@yandex.ru>
2024-06-27 05:21:31 +0300

    Refactoring type guards

--- src/shared/interfaces/form/typeGuards.ts
+++ src/shared/interfaces/form/typeGuards.ts
@@ -1,17 +1,14 @@
-import { GenericTypeFields, IFormFields } from "./formFields";
+import { IPropertyCard } from "../property";
+import { GenericTypeFields } from "./formFields";
 
-export const isApartmentProperty = (
-  Property: IFormFields<GenericTypeFields>
-): Property is IFormFields<GenericTypeFields.Apartment> => Property.category === "Квартира";
+export const isApartmentProperty = (Property: IPropertyCard): Property is IPropertyCard<GenericTypeFields.Apartment> =>
+  Property.category === "Квартира";
 
-export const isHouseProperty = (
-  Property: IFormFields<GenericTypeFields>
-): Property is IFormFields<GenericTypeFields.House> => Property.category === "Дом";
+export const isHouseProperty = (Property: IPropertyCard): Property is IPropertyCard<GenericTypeFields.House> =>
+  Property.category === "Дом";
 
-export const isGarageProperty = (
-  Property: IFormFields<GenericTypeFields>
-): Property is IFormFields<GenericTypeFields.Garage> => Property.category === "Гараж";
+export const isGarageProperty = (Property: IPropertyCard): Property is IPropertyCard<GenericTypeFields.Garage> =>
+  Property.category === "Гараж";
 
-export const isPlotProperty = (
-  Property: IFormFields<GenericTypeFields>
-): Property is IFormFields<GenericTypeFields.Plot> => Property.category === "Участок";
+export const isPlotProperty = (Property: IPropertyCard): Property is IPropertyCard<GenericTypeFields.Plot> =>
+  Property.category === "Участок";
